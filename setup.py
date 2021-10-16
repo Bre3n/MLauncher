c7226e015fd04d97cdf2ff5ab3906100d77c81ee
Bre3n <mwgoinda@gmail.com>
2021-10-16 21:17:54 +0200

    first

---
+++ setup.py
@@ -0,0 +1,215 @@
+def downloader(url, path, var):
+    with open(path, "wb") as f:
+        response = requests.get(url, stream=True)
+        total = response.headers.get("content-length")
+
+        if total is None:
+            f.write(response.content)
+        else:
+            if var == 1:
+                downloaded = 0
+                total = int(total)
+                for data in response.iter_content(
+                    chunk_size=max(int(total / 1000), 1024 * 1024)
+                ):
+                    downloaded += len(data)
+                    f.write(data)
+                    done = int(50 * downloaded / total)
+                    sys.stdout.write(
+                        "\r[{}{}]{}{}".format(
+                            "█" * done,
+                            "." * (50 - done),
+                            f" {round(100*downloaded/total)}%",
+                            f" {round(downloaded/1000)}KB/{round(total/1000)}KB  ",
+                        )
+                    )
+                    sys.stdout.flush()
+            else:
+                downloaded = 0
+                total = int(total)
+                for data in response.iter_content(
+                    chunk_size=max(int(total / 1000), 1024 * 1024)
+                ):
+                    f.write(data)
+
+
+def pliki(pathh):
+    i = 1
+    if path.exists(pathh) == False:
+        os.mkdir(pathh)
+    if path.exists(f"{pathh}/SORT_BY_MODIFICATION_TIME.txt") == False:
+        f = open(f"{pathh}/SORT_BY_MODIFICATION_TIME.txt", "w")
+        f.write("")
+        f.close()
+    if path.exists(f"{pathh}/{now_date}-9.log") == True:
+        os.remove(f"{pathh}/{now_date}-9.log")
+    if path.exists(f"{pathh}/{now_date}.log") == True:
+        while path.exists(f"{pathh}/{now_date}-{i}.log") == True:
+            i = i + 1
+        os.rename(f"{pathh}/{now_date}.log", f"{pathh}/{now_date}-{i}.log")
+    else:
+        if path.exists(f"{pathh}/latest.log") == True:
+            os.rename(
+                f"{pathh}/latest.log",
+                f"{pathh}/{now_date}.log",
+            )
+
+
+if __name__ == "__main__":
+    import os
+    import subprocess
+    import sys
+    import time
+    from os import path
+    import datetime
+    import logging
+
+    try:
+        import doctext
+        import requests
+        import PySide2
+        import zipfile
+        import coloredlogs
+    except ImportError:
+        os.system(f"pip install doctext")
+        os.system(f"pip install requests")
+        os.system(f"pip install PySide2")
+        os.system(f"pip install zipfile")
+        os.system(f"pip install coloredlogs")
+        os.execv(sys.executable, ["python"] + sys.argv)
+
+    os.system("cls")
+
+    now_date = str(datetime.date.today())
+    user = os.getlogin()
+    buffor = False
+    sciezka = f"C:/Users/{user}/AppData/Roaming/.mlauncher"
+    temp = f"C:/Users/{user}/AppData/Local/Temp"
+
+    sciezkaver = f"{sciezka}/bin"
+    docxFile = f"{sciezkaver}/vers.docx"
+    if path.exists(sciezka) == False:
+        os.mkdir(sciezka)
+    if path.exists(f"{sciezka}/bin") == False:
+        os.mkdir(f"{sciezka}/bin")
+
+    pliki(f"{sciezkaver}/logs-setup")
+    logging.Formatter(fmt="%(asctime)s.%(msecs)03d", datefmt="%Y-%m-%d,%H:%M:%S")
+    logging.basicConfig(
+        filename=f"{sciezkaver}/logs-setup/latest.log",
+        format="%(asctime)s %(message)s",
+        filemode="w",
+    )
+    logger = logging.getLogger(__name__)
+    coloredlogs.install(level="DEBUG", logger=logger)
+
+    logger.debug(f"DEBUG: Starting")
+
+    downloader(
+        "https://www.dropbox.com/scl/fi/ty1kcmk35sica88nqxhmi/version.docx?dl=1&rlkey=yozszygj4d2w1dzjxt2cesb3d",
+        docxFile,
+        0,
+    )
+    doc_text = doctext.DocFile(doc=docxFile)
+    doctext = doc_text.get_text()
+    txtFile = f"{sciezkaver}/vers.txt"
+    fdoc = open(txtFile, "w")
+    fdoc.write(doctext)
+    fdoc.close()
+    logger.debug(f"DEBUG: Downloaded version file")
+    os.remove(f"{sciezkaver}/vers.docx")
+    f = open(txtFile, "r")
+    line_count = 0
+    for line in f:
+        if line != "\n":
+            line_count += 1
+    with open(txtFile) as f:
+        content = f.readlines()
+        content = [x.strip() for x in content]
+    for i in range(int(line_count) + 1):
+        content.remove("")
+    if path.exists(f"{sciezkaver}/ver.txt") == True:
+        os.remove(f"{sciezkaver}/ver.txt")
+    f = open(f"{sciezkaver}/ver.txt", "a")
+    for i in range(len(content)):
+        f.write(f"{content[i]}\n")
+    f.close()
+    os.remove(f"{sciezkaver}/vers.txt")
+
+    if path.exists(f"{sciezkaver}/version.txt") == False:
+        f = open(f"{sciezkaver}/version.txt", "w")
+        for i in range(int(len(content)/2)):
+            f.write(f"{content[i]}\n")
+        f.close()
+        logger.info(f"INFO: Version file didn't exist")
+        logger.warning(f"WARNING: Restarting!")
+        os.execv(sys.executable, ["python"] + sys.argv)
+    else:
+        logger.debug(f"DEBUG: Starting version comparing")
+        with open(f"{sciezkaver}/version.txt") as f:
+            contentv = f.readlines()
+            contentv = [x.strip() for x in contentv]
+        if int(len(content)/2) != int(len(contentv)/2):
+            logger.critical(f"CRITICAL: FILES DON'T HAVE SAME NUMER OF LINES!")
+            logger.debug(f"DEBUG: Overwritting wrong file with 'None' lines")
+            f = open(f"{sciezkaver}/version.txt", "w")
+            for i in range(int(len(content)/2)):
+                f.write("None\n")
+        with open(f"{sciezkaver}/version.txt") as f:
+            contentv = f.readlines()
+            contentv = [x.strip() for x in contentv]
+        for i in range(int(len(content) / 2)):
+            if content[i] != contentv[i]:
+                logger.warning(
+                    f"WARNING: Lines didn't match   {content[i]}!={contentv[i]}"
+                )
+                bufor = content[i]
+                bufor = bufor.split("&")
+                checkzip = bufor[0].split(".")
+                logger.debug(f"DEBUG: Downloading   {bufor[0]}")
+                downloader(
+                    content[i + (int(len(content) / 2))],
+                    f"{sciezkaver}/{bufor[0]}",
+                    1,
+                )
+                print(" ")
+                logger.debug(f"DEBUG: Download complete")
+                if checkzip[1] == "zip":
+                    logger.info(f"INFO: Detected zip file. Extractings")
+                    with zipfile.ZipFile(f"{sciezkaver}/{bufor[0]}", "r") as zipObj:
+                        zipObj.extractall(f"{sciezkaver}/{checkzip[0]}")
+                    os.remove(f"{sciezkaver}/{bufor[0]}")
+        f = open(f"{sciezkaver}/version.txt", "w")
+        for i in range(len(content)):
+            f.write(f"{content[i]}\n")
+        f.close()
+        logger.debug(f"DEBUG: Overwritting old version file")
+        os.remove(f"{sciezkaver}/ver.txt")
+        logger.debug(f"DEBUG: Comparing files in directory")
+        for i in range(int(len(content) / 2)):
+            bufor = content[i]
+            bufor = bufor.split("&")
+            checkzip = bufor[0].split(".")
+            buforr = bufor[0]
+            if checkzip[1] == "zip":
+                bufor[0] = checkzip[0]
+            if path.exists(f"{sciezkaver}/{bufor[0]}") == False:
+                logger.warning(
+                    f"WARNING: There is no file named   {bufor[0]}   in directory"
+                )
+                logger.debug(f"DEBUG: Downloading   {bufor[0]}")
+                downloader(
+                    content[i + (int(len(content) / 2))],
+                    f"{sciezkaver}/{buforr}",
+                    1,
+                )
+                print(" ")
+                logger.debug(f"DEBUG: Download complete")
+                if checkzip[1] == "zip":
+                    logger.info(f"INFO: Detected zip file. Extracting")
+                    with zipfile.ZipFile(f"{sciezkaver}/{buforr}", "r") as zipObj:
+                        zipObj.extractall(f"{sciezkaver}/{checkzip[0]}")
+                    os.remove(f"{sciezkaver}/{buforr}")
+        logger.debug(f"DEBUG: Program process complete")
+        os.chdir(f"{sciezkaver}/")
+        subprocess.call(["python", f"{sciezkaver}/main.py"])
---
+++ ui_function.py
@@ -0,0 +1,312 @@
+from main import *  # IMPORTING THE MAIN.PY FILE
+
+import webbrowser
+
+
+GLOBAL_STATE = 0  # NECESSERY FOR CHECKING WEATHER THE WINDWO IS FULL SCREEN OR NOT
+GLOBAL_TITLE_BAR = (
+    True  # NECESSERY FOR CHECKING WEATHER THE WINDWO IS FULL SCREEN OR NOT
+)
+init = False  # NECRESSERY FOR INITITTION OF THE WINDOW.
+
+# tab_Buttons = ['bn_home', 'bn_bug', 'bn_android', 'bn_cloud'] #BUTTONS IN MAIN TAB
+# android_buttons = ['bn_android_contact', 'bn_android_game', 'bn_android_clean', 'bn_android_world'] #BUTTONS IN ANDROID STACKPAGE
+
+# THIS CLASS HOUSES ALL FUNCTION NECESSERY FOR OUR PROGRAMME TO RUN.a
+class UIFunction(MainWindow):
+
+    # ----> INITIAL FUNCTION TO LOAD THE FRONT STACK WIDGET AND TAB BUTTON I.E. HOME PAGE
+    # INITIALISING THE WELCOME PAGE TO: HOME PAGE IN THE STACKEDWIDGET, SETTING THE BOTTOM LABEL AS THE PAGE NAME, SETTING THE BUTTON STYLE.
+    def initStackTab(self):
+        global init
+        if init == False:
+            self.ui.stackedWidget.setCurrentWidget(self.ui.page_home)
+            self.ui.lab_tab.setText("Home")
+            self.ui.frame_home.setStyleSheet("background:rgb(91,90,90)")
+            init = True
+
+    ################################################################################################
+
+    # ------> SETING THE APPLICATION NAME IN OUR CUSTOME MADE TAB, WHERE LABEL NAMED: lab_appname()
+    def labelTitle(self, appName):
+        self.ui.lab_appname.setText(appName)
+
+    ################################################################################################zxc
+
+    ################################################################################################
+
+    # ----> RETURN STATUS MAX OR RESTROE
+    # NECESSERY OFR THE MAXIMISE FUNCTION TRO WORK.
+    def returStatus():
+        return GLOBAL_STATE
+
+    def setStatus(status):
+        global GLOBAL_STATE
+        GLOBAL_STATE = status
+
+    # -----> DEFAULT ACTION FUNCTIONa
+    def constantFunction(self):
+        # -----> DOUBLE CLICK RESULT IN MAXIMISE OF WINDOW
+        def maxDoubleClick(stateMouse):
+            if stateMouse.type() == QtCore.QEvent.MouseButtonDblClick:
+                QtCore.QTimer.singleShot(250, lambda: UIFunction.maximize_restore(self))
+
+        # ----> REMOVE NORMAL TITLE BAR
+        if True:
+            self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
+            self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
+            self.ui.frame_appname.mouseDoubleClickEvent = maxDoubleClick
+
+        # SINCE THERE IS NO WINDOWS TOPBAR, THE CLOSE MIN, MAX BUTTON ARE ABSENT AND SO THERE IS A NEED FOR THE ALTERNATIVE BUTTONS IN OUR
+        # DIALOG BOX, WHICH IS CARRIED OUT BY THE BELOW CODE
+        # -----> MINIMIZE BUTTON FUNCTION
+        self.ui.bn_min.clicked.connect(lambda: self.showMinimized())
+
+        # -----> CLOSE APPLICATION FUNCTION BUTTON
+        self.ui.bn_close.clicked.connect(lambda: self.close())
+
+    ################################################################################################################
+
+    # ----> BUTTON IN TAB PRESSED EXECUTES THE CORRESPONDING PAGE IN STACKEDWIDGET PAGES
+    def buttonPressed(self, buttonName):
+
+        index = self.ui.stackedWidget.currentIndex()
+
+        # ------> THIS LINE CLEARS THE BG OF PREVIOUS TABS I.E. FROM THE LITER COLOR TO THE SAME BG COLOR I.E. TO CHANGE THE HIGHLIGHT.
+        for each in self.ui.frame_bottom_west.findChildren(QFrame):
+            each.setStyleSheet("background:rgb(51,51,51)")
+
+        if buttonName == "bn_home":
+            if self.ui.frame_bottom_west.width() == 80 and index != 0:
+                self.ui.stackedWidget.setCurrentWidget(self.ui.page_home)
+                self.ui.lab_tab.setText("Home")
+                self.ui.frame_home.setStyleSheet(
+                    "background:rgb(91,90,90)"
+                )  # SETS THE BACKGROUND OF THE CLICKED BUTTON TO LITER COLOR THAN THE REST
+
+            elif (
+                self.ui.frame_bottom_west.width() == 160 and index != 1
+            ):  # ABOUT PAGE STACKED WIDGET
+                self.ui.stackedWidget.setCurrentWidget(self.ui.page_about_home)
+                self.ui.lab_tab.setText("About > Home")
+                self.ui.frame_home.setStyleSheet(
+                    "background:rgb(91,90,90)"
+                )  # SETS THE BACKGROUND OF THE CLICKED BUTTON TO LITER COLOR THAN THE REST
+
+        elif buttonName == "bn_bug":
+            if self.ui.frame_bottom_west.width() == 80 and index != 5:
+                self.ui.stackedWidget.setCurrentWidget(self.ui.page_bug)
+                self.ui.lab_tab.setText("Game")
+                self.ui.frame_bug.setStyleSheet(
+                    "background:rgb(91,90,90)"
+                )  # SETS THE BACKGROUND OF THE CLICKED BUTTON TO LITER COLOR THAN THE REST
+
+            elif (
+                self.ui.frame_bottom_west.width() == 160 and index != 4
+            ):  # ABOUT PAGE STACKED WIDGET
+                self.ui.stackedWidget.setCurrentWidget(self.ui.page_about_bug)
+                self.ui.lab_tab.setText("About > Game")
+                self.ui.frame_bug.setStyleSheet(
+                    "background:rgb(91,90,90)"
+                )  # SETS THE BACKGROUND OF THE CLICKED BUTTON TO LITER COLOR THAN THE REST
+
+        elif buttonName == "bn_android":
+            if self.ui.frame_bottom_west.width() == 80 and index != 7:
+                self.ui.stackedWidget.setCurrentWidget(self.ui.page_android)
+                self.ui.lab_tab.setText("Settings")
+                self.ui.frame_android.setStyleSheet(
+                    "background:rgb(91,90,90)"
+                )  # SETS THE BACKGROUND OF THE CLICKED BUTTON TO LITER COLOR THAN THE REST
+                UIFunction.androidStackPages(self, "page_contact")
+
+            elif (
+                self.ui.frame_bottom_west.width() == 160 and index != 3
+            ):  # ABOUT PAGE STACKED WIDGET
+                self.ui.stackedWidget.setCurrentWidget(self.ui.page_about_android)
+                self.ui.lab_tab.setText("About > Settings")
+                self.ui.frame_android.setStyleSheet(
+                    "background:rgb(91,90,90)"
+                )  # SETS THE BACKGROUND OF THE CLICKED BUTTON TO LITER COLOR THAN THE REST
+
+        # ADD ANOTHER ELIF STATEMENT HERE FOR EXECTUITING A NEW MENU BUTTON STACK PAGE.
+
+    ########################################################################################################################
+
+    # ----> STACKWIDGET EACH PAGE FUNCTION PAGE FUNCTIONS
+    # CODE TO PERFOMR THE TASK IN THE STACKED WIDGET PAGE
+    # WHAT EVER WIDGET IS IN THE STACKED PAGES ITS ACTION IS EVALUATED HERE AND THEN THE REST FUNCTION IS PASSED.
+    def stackPage(self):
+
+        ######### PAGE_HOME ############# BELOW DISPLAYS THE FUNCTION OF WIDGET, LABEL, PROGRESS BAR, E.T.C IN STACKEDWIDGET page_HOME
+
+        ######### PAGE_BUG ############## BELOW DISPLAYS THE FUNCTION OF WIDGET, LABEL, PROGRESS BAR, E.T.C IN STACKEDWIDGET page_bug
+        self.ui.bn_bug_start.clicked.connect(
+            lambda: APFunction.addNumbers(
+                self, self.ui.comboBox_bug.currentText(), True
+            )
+        )
+
+        #########PAGE ANDROID WIDGET AND ITS STACKANDROID WIDGET PAGES
+        self.ui.bn_android_contact.clicked.connect(
+            lambda: UIFunction.androidStackPages(self, "page_contact")
+        )
+        self.ui.bn_android_game.clicked.connect(
+            lambda: UIFunction.androidStackPages(self, "page_game")
+        )
+        self.ui.bn_android_clean.clicked.connect(
+            lambda: UIFunction.androidStackPages(self, "page_clean")
+        )
+        self.ui.bn_android_world.clicked.connect(
+            lambda: UIFunction.androidStackPages(self, "page_world")
+        )
+
+        ######ANDROID > PAGE CONTACT >>>>>>>>>>>>>>>>>>>>
+        self.ui.bn_android_contact_delete.clicked.connect(
+            lambda: APFunction.deleteContact(self)
+        )
+        self.ui.line_checkbox_arg.clicked.connect(lambda: APFunction.checkBox_arg(self))
+        self.ui.line_checkbox_rpc.clicked.connect(lambda: APFunction.checkBox_rpc(self))
+        self.ui.bn_android_contact_edit.clicked.connect(
+            lambda: APFunction.editable(self)
+        )
+        # asd
+        self.ui.bn_android_contact_save.clicked.connect(
+            lambda: APFunction.saveContact(self)
+        )
+
+        #######ANDROID > PAGE GAMEPAD >>>>>>>>>>>>>>>>>>>
+        self.ui.textEdit_gamepad.setVerticalScrollBar(
+            self.ui.vsb_gamepad
+        )  # SETTING THE TEXT FILED AREA A SCROLL BAR
+        self.ui.textEdit_gamepad.setText("Type Here Something, or paste something here")
+
+        ######ANDROID > PAGE CLEAN >>>>>>>>>>>>>>>>>>>>>>
+        # NOTHING HERE
+        self.ui.horizontalSlider_2.valueChanged.connect(
+            lambda: print("Slider: Horizondal: ", self.ui.horizontalSlider_2.value())
+        )  # CHECK WEATHER THE SLIDER IS MOVED OR NOT
+
+        ##########PAGE: ABOUT HOME #############
+        self.ui.text_about_home.setVerticalScrollBar(self.ui.vsb_about_home)
+
+        ##########PAGE: INFORMATION #############
+
+        self.ui.pushButton_github.clicked.connect(
+            lambda: APFunction.pushButton_github(self)
+        )
+
+    ################################################################################################################################
+
+    # -----> FUNCTION TO SHOW CORRESPONDING STACK PAGE WHEN THE ANDROID BUTTONS ARE PRESSED: CONTACT, GAME, CLOUD, WORLD
+    # SINCE THE ANDROID PAGE AHS A SUB STACKED WIDGET WIT FOUR MORE BUTTONS, ALL THIS 4 PAGES CONTENT: BUTTONS, TEXT, LABEL E.T.C ARE INITIALIED OVER HERE.
+    def androidStackPages(self, page):
+        # ------> THIS LINE CLEARS THE BG COLOR OF PREVIOUS TABS
+        for each in self.ui.frame_android_menu.findChildren(QFrame):
+            each.setStyleSheet("background:rgb(51,51,51)")
+
+        if page == "page_contact":
+            self.ui.stackedWidget_android.setCurrentWidget(self.ui.page_android_contact)
+            self.ui.lab_tab.setText("Settings > Profile")
+            self.ui.frame_android_contact.setStyleSheet("background:rgb(91,90,90)")
+
+        elif page == "page_game":
+            self.ui.stackedWidget_android.setCurrentWidget(self.ui.page_android_game)
+            self.ui.lab_tab.setText("Settings > GamePad")
+            self.ui.frame_android_game.setStyleSheet("background:rgb(91,90,90)")
+
+        elif page == "page_clean":
+            self.ui.stackedWidget_android.setCurrentWidget(self.ui.page_android_clean)
+            self.ui.lab_tab.setText("Settings > Clean")
+            self.ui.frame_android_clean.setStyleSheet("background:rgb(91,90,90)")
+
+        elif page == "page_world":
+            self.ui.stackedWidget_android.setCurrentWidget(self.ui.page_android_world)
+            self.ui.lab_tab.setText("Settings > Information")
+            self.ui.frame_android_world.setStyleSheet("background:rgb(91,90,90)")
+
+        # ADD A ADDITIONAL ELIF STATEMNT WITH THE SIMILAR CODE UP ABOVE FOR YOUR NEW SUBMENU BUTTON IN THE ANDROID STACK PAGE.
+
+    ##############################################################################################################
+
+
+# ------> CLASS WHERE ALL THE ACTION OF TH SOFTWARE IS PERFORMED:
+# THIS CLASS IS WHERE THE APPLICATION OF THE UI OR THE BRAINOF THE SOFTWARE GOES
+# UNTILL NOW WE SEPCIFIED THE BUTTON CLICKS, SLIDERS, E.T.C WIDGET, WHOSE APPLICATION IS EXPLORED HERE. THOSE FUNCTION WHEN DONE IS
+# REDIRECTED TO THIS AREA FOR THE PROCESSING AND THEN THE RESULT ARE EXPOTED.
+# REMEMBER THE SOFTWARE UI HAS A FUNCTION WHOSE CODE SHOULD BE HERE
+class APFunction:
+    # -----> ADDING NUMBER TO ILLUSTRATE THE CAPABILITY OF THE PROGRESS BAR WHEN THE 'START' BUTTON IS PRESSED
+
+    ###########################
+
+    # -----> FUNCTION IN ACCOUNT OF CONTACT PAGE IN ANDROID MENU
+    def editable(self):
+        self.ui.line_android_name.setEnabled(True)
+        self.ui.line_android_adress.setEnabled(True)
+        self.ui.line_android_email.setEnabled(True)
+        self.ui.line_checkbox_arg.setEnabled(True)
+        self.ui.line_checkbox_rpc.setEnabled(True)
+
+        self.ui.bn_android_contact_save.setEnabled(True)
+        self.ui.bn_android_contact_edit.setEnabled(False)
+        self.ui.bn_android_contact_delete.setEnabled(False)
+        self.ui.bn_home.setEnabled(False)
+        self.ui.bn_bug.setEnabled(False)
+        self.ui.bn_android.setEnabled(False)
+        self.ui.bn_android_contact.setEnabled(False)
+        self.ui.bn_android_game.setEnabled(False)
+        self.ui.bn_android_clean.setEnabled(False)
+        self.ui.bn_android_world.setEnabled(False)
+
+    # -----> FUNCTION TO SAVE THE MODOFOED TEXT FIELD
+    def saveContact(self):
+        self.ui.lab_user.setText(self.ui.line_android_name.text())
+        self.ui.lab_home_username.setText(self.ui.line_android_name.text())
+        self.ui.line_android_name.setEnabled(False)
+        self.ui.line_android_adress.setEnabled(False)
+        self.ui.line_android_email.setEnabled(False)
+        self.ui.line_checkbox_arg.setEnabled(False)
+        self.ui.line_checkbox_rpc.setEnabled(False)
+
+        self.ui.bn_android_contact_save.setEnabled(False)
+        self.ui.bn_android_contact_edit.setEnabled(True)
+        self.ui.bn_android_contact_delete.setEnabled(True)
+        self.ui.bn_home.setEnabled(True)
+        self.ui.bn_bug.setEnabled(True)
+        self.ui.bn_android.setEnabled(True)
+        self.ui.bn_android_contact.setEnabled(True)
+        self.ui.bn_android_game.setEnabled(True)
+        self.ui.bn_android_clean.setEnabled(True)
+        self.ui.bn_android_world.setEnabled(True)
+
+    def deleteContact(self):
+        self.dialogexec(
+            "Warning",
+            "Saved settings will be deleted, Do you want to continue?",
+            "icons/1x/errorAsset 55.png",
+            "Cancel",
+            "Yes",
+        )
+
+    def checkBox_arg(self):
+        if self.ui.line_checkbox_arg.isChecked() == False:
+            self.ui.line_checkbox_arg.setText("Unchecked")
+        else:
+            self.ui.line_checkbox_arg.setText("Checked")
+            self.errorexec(
+                "Recommended if you have alocated more than 2GB RAM",
+                "icons/1x/errorAsset 55.png",
+                "Ok",
+            )
+
+    def checkBox_rpc(self):
+        if self.ui.line_checkbox_rpc.isChecked() == False:
+            self.ui.line_checkbox_rpc.setText("Unchecked")
+        else:
+            self.ui.line_checkbox_rpc.setText("Checked")
+
+    def pushButton_github(self):
+        webbrowser.open("https://github.com/Bre3n")
+
+
+#######################################as########################################################################################################################
